6c70a23daa08d4c1a33ad93374412154eea103ab
sawyersieja <sawyersieja@protonmail.com>
2024-05-28 19:05:49 -0700

    Fixed hardhat deployment not getting proper deployment address.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,22 @@
 // scripts/deploy.js
-
 async function main() {
     // Get the contract factory.
+    console.log("Starting smart contract deployment process...");
     const CoinFlip = await ethers.getContractFactory("CoinFlip");
-    console.log("Deploying CoinFlip...");
+    console.log("Contract factory loaded.");
 
     // Deploy the contract.
+    console.log("Deploying the CoinFlip contract to the blockchain...");
     const coinFlip = await CoinFlip.deploy();
-    await coinFlip.deployed;
-    //await coinFlip.deployTransaction.wait();
+    
+    // This line of code makes it so we wait for 1 block confirmation before proceeding.
+    await coinFlip.deploymentTransaction().wait(1);
+    console.log("Deployment transaction has been successfully mined.");
 
-    // The contract is already deployed at this point.
-    console.log("CoinFlip deployed to:", coinFlip.address);
+    // I was getting a lot of issues when trying to use coinFlip.address.
+    // I found a solution to use .target instead.
+    // https://stackoverflow.com/questions/77117163/unable-to-get-the-deployed-contract-address-using-etherjs
+    console.log("CoinFlip deployed to this contract address:", coinFlip.target);  
   }
   
   main()
